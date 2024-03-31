9e227776f3bc793d4ee26f8346bd44f68ddee860
gunwoong <gwj0421@gmail.com>
2024-03-31 16:18:42 +0900

    init commit

---
+++ src/main/java/kimchisoup/matchingjob/MatchingJobApplication.java
@@ -0,0 +1,13 @@
+package kimchisoup.matchingjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchingJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kimchisoup/matchingjob/MatchingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package kimchisoup.matchingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
