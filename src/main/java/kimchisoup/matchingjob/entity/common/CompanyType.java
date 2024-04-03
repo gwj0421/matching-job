3f62d283c7db37cc97be567fbe863aae596344c0
20191777 <20191777@o365.hanbat.ac.kr>
2024-04-03 17:16:35 +0900

    feat: CompanyType enum 수정 - 회사 종류 업데이트

--- src/main/java/kimchisoup/matchingjob/entity/common/CompanyType.java
+++ src/main/java/kimchisoup/matchingjob/entity/common/CompanyType.java
@@ -1,4 +1,15 @@
 package kimchisoup.matchingjob.entity.common;
 
 public enum CompanyType {
+    MAJOR_COMPANY("대기업"),
+    MIDSIZE_COMPANY("중견기업"),
+    SMALL_COMPANY("중소기업"),
+    FOREIGN("외국계"),
+    PUBLIC_ENTERPRISE("공기업"),
+    START_UP("스타트업");
+
+    private final String desc;
+    CompanyType(String desc){
+        this.desc = desc;
+    }
 }
